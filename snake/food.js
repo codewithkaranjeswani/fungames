71261db6faf844a8fecd8ca33494ea3878ebe830
Karan Jeswani <34395853+codewithkaranjeswani@users.noreply.github.com>
2018-03-06 20:41:16 +0530

    Add files via upload
    completed!

--- snake/food.js
+++ snake/food.js
@@ -1,17 +1,18 @@
-function Food()
-{
-  this.x = floor(random(snap, width/snap)) * snap;
-  this.y = floor(random(snap, height/snap)) * snap;
+function Food() {
+  this.x = floor(random(1, width / snap - 1)) * snap;
+  this.y = floor(random(1, height / snap - 1)) * snap;
 
-  this.show = function()
-  {
+  this.show = function() {
     fill(255, 0, 0);
     noStroke();
     rect(this.x, this.y, snap, snap);
   }
-  this.changelocation = function()
-  {
-    this.x = floor(random(1, width/snap - 2)) * snap;
-    this.y = floor(random(1, height/snap - 2)) * snap;
+  this.changelocation = function(snaketail) {
+    this.x = floor(random(1, width / snap - 1)) * snap;
+    this.y = floor(random(1, height / snap - 1)) * snap;
+    for (let i = 0; i < snaketail.length; i++) {
+      if (this.x === snaketail[i].x && this.y === snaketail[i].y)
+        this.changelocation(snaketail);
+    }
   }
 }
--- snake/sketch.js
+++ snake/sketch.js
@@ -1,47 +1,48 @@
 // the snake game
-var cobra;
-var snap;
-var apple;
-function setup()
-{
-  frameRate(10);
+let snap = 20;
+let apple, cobra;
+let score = 0;
+
+function setup() {
   createCanvas(600, 600);
-  snap = 20;
-  cobra = new Snake();
+  frameRate(10);
   apple = new Food();
+  cobra = new Snake();
 }
 
-function draw()
-{
+function draw() {
   background(80, 20, 180);
   makeBorder();
   apple.show();
   cobra.update();
   cobra.show();
-  if(cobra.eatsfood(apple.x, apple.y))
-  {
-    apple.changelocation();
+  if (cobra.eatsfood(apple.x, apple.y)) {
+    apple.changelocation(cobra.tail);
+    score++;
+    console.log(score);
+  }
+  if (cobra.defeated()) {
+    console.log("game over! final score = ", score, "!");
+    exit();
   }
 }
 
- function makeBorder()
-{
+function makeBorder() {
   fill(0, 25, 75);
   noStroke();
-  rect(0, 0 ,20, height);
-  rect(width - 20, 0, 20, height);
-  rect(0, 0, width, 20);
-  rect(0, height - 20, width, 20);
+  rect(0, 0, snap, height);
+  rect(width - snap, 0, snap, height);
+  rect(0, 0, width, snap);
+  rect(0, height - snap, width, snap);
 }
 
-function keyPressed()
-{
-  if(keyCode === RIGHT_ARROW)
+function keyPressed() {
+  if (keyCode === RIGHT_ARROW)
     cobra.dir(1, 0);
-  else if(keyCode === LEFT_ARROW)
+  else if (keyCode === LEFT_ARROW)
     cobra.dir(-1, 0);
-  else if(keyCode === UP_ARROW)
+  else if (keyCode === UP_ARROW)
     cobra.dir(0, -1);
-  else if(keyCode === DOWN_ARROW)
+  else if (keyCode === DOWN_ARROW)
     cobra.dir(0, 1);
 }
