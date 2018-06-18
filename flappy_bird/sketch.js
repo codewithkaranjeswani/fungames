f950e815737e6764dbef431abc5a4ab69ebe5725
Karan Jeswani <34395853+codewithkaranjeswani@users.noreply.github.com>
2018-06-18 15:30:09 +0530

    added score and some corrections

--- flappy_bird/sketch.js
+++ flappy_bird/sketch.js
@@ -1,41 +1,42 @@
-// working on fullScreen version:
-// make score work properly
-// working on the collision function and restart game when its over
-// also work on restarting the game
+// working on fullScreen version: done! test on phone!
+// working on the collision function
+// also work on restarting the game! done!
 let birdy;
 let pipy = [];
-let score = 0;
-let allowed = true;
-let previndex = 0;
 let gameover = false;
 let BORDER;
-let start = false;
-// let music;
-
-// function preload() {
-//   music = loadSound('Edward Maya  Vika Jigulina - Stereo Love (Jay Latune Remix).mp3');
-// }
+let widthdef;
+let heightdef;
+let score = 0;
+let d;
+let dprev;
+let counter;
 
 function setup() {
-  createCanvas(1530, 860);
+  widthdef = window.screen.width - 3;
+  heightdef = window.screen.height - 3.7;
+  dprev = widthdef;
+  counter = 0;
+  createCanvas(widthdef, heightdef);
   BORDER = height / 40;
   birdy = new Bird();
   pipy.push(new Pipe());
-  noLoop();
-  // music.play();
 }
 
 function draw() {
-  // if (!music.isPlaying()) {
-  //   music.play();
-  // }
+  counter += 1;
+  // console.log(counter);
   drawingStuff();
-  if (start) {
+  if (counter == 1) {
+    noLoop();
+  }
+  if (counter >= 1) {
     // game logic
+    updateScore(pipy, birdy);
     for (let i = pipy.length - 1; i >= 0; i--) {
       pipy[i].update();
     }
-    if (frameCount % 75 == 0) {
+    if (counter % 75 == 0) {
       pipy.push(new Pipe());
     }
     birdy.update();
@@ -45,14 +46,7 @@ function draw() {
       gameover = true;
       noLoop();
     }
-    if (pipy[index].x < birdy.x && allowed) {
-      score++;
-      allowed = false;
-    }
-    if (index == 1) {
-      allowed = true;
-    }
-    // deleting stuff from the array
+    // deleting stuff from the pipy array
     for (let i = pipy.length - 1; i >= 0; i--) {
       if (pipy[i].x + pipy[i].thick < -5) {
         pipy.splice(i, 1);
@@ -62,8 +56,44 @@ function draw() {
   }
 }
 
+function updateScore(pipy, birdy) {
+  // use different distances for keeping score and checking collision
+  let scoreIndex = 0;
+  let min_s = width;
+
+  for (let i = 0; i < pipy.length; i++) {
+    // bird-end to pipe-start distance is d, used for keeping score!
+    let scoredis = pipy[i].x - birdy.x - birdy.size / 2;
+    if (scoredis < min_s && scoredis >= 0) {
+      min_s = scoredis;
+      d = scoredis;
+      scoreIndex = i;
+    }
+  }
+  if (d > dprev) {
+    score += 1;
+  }
+  dprev = d;
+}
+
+function closestPipe(pipy, birdy) {
+  // a pipe is said to be closest to the bird as long as
+  // the bird's start passes through the end of the pipe.
+  // use different distances for keeping score and checking collision
+  let minIndex = 0;
+  let min_d = width;
+  for (let i = 0; i < pipy.length; i++) {
+    // pipe-end to bird-start distance is dis, used for collision detection!
+    let dis = pipy[i].x + pipy[i].thick - (birdy.x - birdy.size / 2);
+    if (dis < min_d && dis >= 0) {
+      min_d = dis;
+      minIndex = i;
+    }
+  }
+  return minIndex;
+}
+
 function drawingStuff() {
-  // drawing stuff
   background(51);
   birdy.show();
   for (let i = pipy.length - 1; i >= 0; i--) {
@@ -93,27 +123,29 @@ function drawingStuff() {
 }
 
 function keyPressed() {
-  if (key == ' ' && frameCount == 1) {
+  if (key == ' ' && counter == 1) {
     birdy.jump();
     loop();
   }
   if (key == ' ') {
     birdy.jump();
   }
-
-  if (key == ' ' && !start) {
-    start = true;
+  if (key == 'R') {
+    birdy = new Bird();
+    pipy = [];
+    pipy.push(new Pipe());
+    dprev = widthdef;
+    score = 0;
+    counter = 0;
+    // console.log(counter);
+    if (!gameover) {
+      noLoop();
+    } else {
+      loop();
+    }
+    gameover = false;
+    drawingStuff();
   }
-
-  // if (key == ' ' && gameover) {
-  // pipy.length = 0;
-  // pipy.push(new Pipe());
-  // score = 0;
-  // allowed = true;
-  // previndex = 0;
-  // loop();
-  // gameover = false;
-  // }
 }
 
 function mousePressed() {
@@ -122,19 +154,6 @@ function mousePressed() {
     loop();
   }
   birdy.jump();
-}
-
-function closestPipe(pipy, birdy) {
-  let minIndex = 0;
-  let min_d = width;
-  for (let i = 0; i < pipy.length; i++) {
-    let d = pipy[i].x + pipy[i].thick - birdy.x;
-    if (d < min_d && d >= 0) {
-      min_d = d;
-      minIndex = i;
-    }
-  }
-  return minIndex;
 }
 
 class Pipe {
@@ -147,15 +166,15 @@ class Pipe {
     this.x = width + this.thick;
   }
 
-  update() {
-    this.x -= this.speed;
-  }
-
   show() {
-    fill(100, 255, 100);
+    fill(100, 255, 100, 100);
     noStroke();
     rect(this.x, BORDER, this.thick, this.ytop);
     rect(this.x, this.ybottom + BORDER, this.thick, height - this.ybottom - 2 * BORDER);
+  }
+
+  update() {
+    this.x -= this.speed;
   }
 
   collides(birdy) {
@@ -180,7 +199,8 @@ class Pipe {
     }
 
     let d = this.x - birdy.x;
-    if (d < birdy.size / 2 && (birdy.y - birdy.size / 2 <= this.ytop + BORDER || birdy.y + birdy.size / 2 >= this.ybottom + BORDER)) {
+    if (d < birdy.size / 2 && (birdy.y - birdy.size / 2 <= this.ytop + BORDER ||
+        birdy.y + birdy.size / 2 >= this.ybottom + BORDER)) {
       // birdy.x = this.x - birdy.size / 2;
       return true;
     } else {
@@ -213,50 +233,51 @@ class Bird {
   jump() {
     this.speed = -10;
   }
-
-  // collides(pipy) {
-  //   if (this.x <= this.size / 2) {
-  //     this.x = this.size / 2;
-  //     return true;
-  //   }
-  //   if (this.x >= width - this.size / 2) {
-  //     this.x = width - this.size / 2;
-  //     this.speed = 0;
-  //     return true;
-  //   }
-  //   if (this.y <= this.size / 2) {
-  //     this.y = this.size / 2;
-  //     this.speed = 0;
-  //     return true;
-  //   }
-  //   if (this.y >= height - this.size / 2) {
-  //     this.y = height - this.size / 2;
-  //     this.speed = 0;
-  //     return true;
-  //   }
-  //   // error in inside top and inside bottom, please check
-  //   for (let pip of pipy) {
-  //
-  //
-  //
-  //     // if (this.x + this.size / 2 >= pip.x && this.x + this.size / 2 <= pip.x + 5 && ((this.y - this.size / 2 <= pip.ytop) || (this.y + this.size / 2 >= pip.ybottom))) {
-  //     //   this.x = pip.x - this.size / 2;
-  //     //   console.log('edge');
-  //     //   return true;
-  //     // } else {
-  //     //   if (this.x + this.size / 2 >= pip.x && this.x + this.size / 2 <= pip.x + pip.thick && ((this.y - this.size / 2 <= pip.ytop) || (this.y + this.size / 2 >= pip.ybottom))) {
-  //     //     if (this.y - this.size / 2 <= pip.ytop) {
-  //     //       this.y = pip.ytop + this.size / 2;
-  //     //       console.log('inside top');
-  //     //     } else if (this.y + this.size / 2 >= pip.ybottom) {
-  //     //       this.y = pip.ybottom - this.size / 2;
-  //     //       console.log('inside bottom');
-  //     //     }
-  //     //     return true;
-  //     //   } else {
-  //     //     return false;
-  //     //   }
-  //     // }
-  //   }
-  // }
 };
+
+///////////////////////////////////////////////////////////////////////////////
+// collides(pipy) {
+//   if (this.x <= this.size / 2) {
+//     this.x = this.size / 2;
+//     return true;
+//   }
+//   if (this.x >= width - this.size / 2) {
+//     this.x = width - this.size / 2;
+//     this.speed = 0;
+//     return true;
+//   }
+//   if (this.y <= this.size / 2) {
+//     this.y = this.size / 2;
+//     this.speed = 0;
+//     return true;
+//   }
+//   if (this.y >= height - this.size / 2) {
+//     this.y = height - this.size / 2;
+//     this.speed = 0;
+//     return true;
+//   }
+//   // error in inside top and inside bottom, please check
+//   for (let pip of pipy) {
+//
+//
+//
+//     // if (this.x + this.size / 2 >= pip.x && this.x + this.size / 2 <= pip.x + 5 && ((this.y - this.size / 2 <= pip.ytop) || (this.y + this.size / 2 >= pip.ybottom))) {
+//     //   this.x = pip.x - this.size / 2;
+//     //   console.log('edge');
+//     //   return true;
+//     // } else {
+//     //   if (this.x + this.size / 2 >= pip.x && this.x + this.size / 2 <= pip.x + pip.thick && ((this.y - this.size / 2 <= pip.ytop) || (this.y + this.size / 2 >= pip.ybottom))) {
+//     //     if (this.y - this.size / 2 <= pip.ytop) {
+//     //       this.y = pip.ytop + this.size / 2;
+//     //       console.log('inside top');
+//     //     } else if (this.y + this.size / 2 >= pip.ybottom) {
+//     //       this.y = pip.ybottom - this.size / 2;
+//     //       console.log('inside bottom');
+//     //     }
+//     //     return true;
+//     //   } else {
+//     //     return false;
+//     //   }
+//     // }
+//   }
+// }
